cd5340fbec25df690aa8e3bc58006736fe191d63
risvinmpm <risvinmp39@gmail.com>
2025-03-05 14:47:43 +0530

    latest

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Dev</span></p>
+                <p>How can i help you today?</p>
+            </div>
+            <div className='cards'>
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main;
