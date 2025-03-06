cfb933b684f77690f7714238f4b2e93f3477b571
risvinmpm <risvinmp39@gmail.com>
2025-03-06 16:28:56 +0530

    latest updates

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,27 +12,35 @@ const Contextprovider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
+    const delayPara = (index,nextWord) => {
+        setTimeout(function(){
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
         setRecentPromt(input)
+        setPrevPrompts(prev=>[...prev,input])
         const response = await run(input)
         let responseArray = response.split("**")
         let newResponse;
-        for(let i =0; i < responseArray.length; i++)
-        {
-            if (i === 0 || i== 1) {
+        for(let i =0; i < responseArray.length; i++){
+            if (i === 0 || i%2 !== 1) {
                 newResponse += responseArray[i];
+            }else {
+                newResponse += "<b>"+responseArray[i]+"</b>";
             }
-            else {
-                newResponse += "<b>"+responseArray[i]+"</b>"
-            }
-
         }
         let newResponse2 = newResponse.split("*").join("</br>")
-        setResultData(newResponse2)
+        let newResponseArray = newResponse2.split(" ");
+        for(let i=0; i<newResponseArray.length;i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ")
+        }
         setLoading(false)
         setInput("")
     }
